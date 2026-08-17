fda66331b8564ab2742eff3d808067a8d33c7a09
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-08-17 19:07:22 +0530

    day3 done

--- day3.py
+++ day3.py
@@ -1,9 +1,12 @@
 import numpy as np
-temps =  np.array([10,20,30])
+#temps =  np.array([10,20,30])
 #print("avg tampertaure:" , np.mean(temps))
-new_temps = temps +5
-print(new_temps)
-print(np.max(new_temps))
-print(np.min(new_temps))
-hot_temps = temps[temps>15]
-print(hot_temps)
+#new_temps = temps +5
+#print(new_temps)
+#print(np.max(new_temps))
+#print(np.min(new_temps))
+#hot_temps = temps[temps>15]
+#print(hot_temps)
+
+temps = np.array([10,20,30,40,50])
+print("btw temps:", temps[1:4])
