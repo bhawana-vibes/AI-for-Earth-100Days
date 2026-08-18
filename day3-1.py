a8966cffb3743dfe48bf8a61dc5a46b56d3902a7
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-08-18 12:29:17 +0530

    day3 done

--- day3-1.py
+++ day3-1.py
@@ -10,3 +10,16 @@ grid = temp[0:2 ,0:2]
 print(grid)
 temps = temp[0:2 , 1: ]
 print(temps)
+
+
+arr = np.array([10, 20, 30, 40, 50, 60])
+grid1 = arr.reshape(2,3)
+print(grid1)
+
+flat_arr = grid1.flatten()
+print(flat_arr)
+
+
+data = np.arange(1,13)
+grid2 = data.reshape(3,4)
+print(grid2)
