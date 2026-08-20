59f47f3227d93cc27d2c2b5abcf37928fe320a7d
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-08-20 13:32:54 +0530

    pandas

--- day6-2.py
+++ day6-2.py
@@ -16,7 +16,33 @@ import pandas as pd
 #plt.show()
 
 
-df = pd.DataFrame({'month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], 'profit': [12000, 18000, 15000, 22000, 28000, 35000]})
+#df = pd.DataFrame({'month': ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], 'profit': [12000, 18000, 15000, 22000, 28000, 35000]})
 
-df.plot(x='month', y='profit', marker='o', grid=True)
+#df.plot(x='month', y='profit', marker='o', grid=True)
+#plt.show()
+
+#df = pd.DataFrame({'salary': [25, 28, 30, 32, 35, 36, 75, 80, 85, 90]})
+
+#df['salary'].plot(kind='hist', bins=10 , edgecolor='black')
+#plt.show()
+
+
+
+#df = pd.DataFrame({
+  #  'experience_years': [1, 2, 3, 4, 5, 6, 7],
+ #   'salary_in_lakhs':  [4, 5, 7, 9, 12, 16, 20]
+#})
+
+#df.plot(kind='scatter', x='experience_years', y='salary_in_lakhs', color='red')
+#plt.show()
+
+
+df = pd.DataFrame({
+    'experience_years': [1, 2, 3, 4, 5, 6],
+    'salary_in_lakhs':  [12, 10, 8, 6, 4, 2]
+})
+
+df.plot(kind='scatter', x='experience_years', y='salary_in_lakhs', marker='v' , s=100 , color='blue')
 plt.show()
+
+
