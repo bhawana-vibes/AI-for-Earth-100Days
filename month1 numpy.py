1dfa362306796ec430f43b8708f2309d0a9ac837
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-08-22 13:17:48 +0530

    month numpy

--- month1 numpy.py
+++ month1 numpy.py
@@ -8,3 +8,15 @@ print("Average (Mean):", np.mean(marks_arr))
 print("Max Marks:", np.max(marks_arr))
 print("Min Marks:", np.min(marks_arr))
 print("Total Sum:", np.sum(marks_arr))
+
+
+prices = np.array([100, 200, 300])
+#grace_prices = prices*2
+print("grace_prices:" ,prices*2)
+
+scores = np.array([40, 75, 90, 30, 85])
+#high_scores = scores[scores>50]
+print("high_scores:" , scores[scores>50])
+
+ages = np.array([12, 25, 17, 30, 15, 40])
+print("highest ages:" ,ages[ages >= 18])
