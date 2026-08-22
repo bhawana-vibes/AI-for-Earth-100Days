9699e8d8d93b5c80b2cdea51b6852eba76936994
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-08-22 12:36:42 +0530

    month

--- month1.py
+++ month1.py
@@ -4,8 +4,14 @@ stu= [
     {"name": "Rohan", "marks": 60}
 ]
 count = 0
+total_marks =0
+total_stu = len(stu)
 for s in stu:
     if s["marks"]>70:
         count = count+1
         print(s["marks"])
+    total_marks = total_marks +s["marks"]
+print(total_marks)
+avg = total_marks/ total_stu
+print("total avg:" , avg)
 print("total count:", count)
