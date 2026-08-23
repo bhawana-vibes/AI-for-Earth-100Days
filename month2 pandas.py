c7e7b32780061b8cc6f34c28cca304f348aebb01
bhawana-vibes <bhawanahiranwal0@gmail.com>
2026-08-23 09:41:53 +0530

    pandas done

--- month2 pandas.py
+++ month2 pandas.py
@@ -6,3 +6,14 @@ data = {
 }
 df = pd.DataFrame(data)
 print(df)
+print(df["Marks"])
+print("highest marks:" , df[df["Marks"]>75])
+df["Final_Marks"] = df["Marks"] + 5
+print(df)
+print(df.groupby("Branch")["Marks"].mean())
+print("stu marks:",df[df["Marks"] < 70])
+print("head:" , df.head())
+print("info:" , df.info())
+print("shape:" , df.shape)
+branch_avg = df.groupby("Branch")["Marks"].mean()
+print("averge:" , branch_avg)
